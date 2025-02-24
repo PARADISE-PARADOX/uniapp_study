97761b5cdbc9579b4b785af162a17ad4c1a4080d
PARADISE-PARADOX <whoamiparadise@gmail.com>
2025-02-25 01:21:12 +0800

    111

---
+++ components/emit-child/emit-child.vue
@@ -0,0 +1,15 @@
+<template>
+	<view>
+		子组件
+		<button @click="$emit('add',Math.random())">btn</button>
+	</view>
+</template>
+
+<script setup>
+	
+	
+</script>
+
+<style lang='scss' scoped>
+
+</style>
---
+++ pages/emit/emit.vue
@@ -0,0 +1,22 @@
+<template>
+  <view class="">
+	<emit-child @add="onAdd"></emit-child>
+	<view>num:{{num}}</view>
+  </view>
+</template>
+
+<script setup>
+	
+import {ref} from 'vue'
+const num = ref(0)
+	
+const onAdd = e =>{
+	console.log(e)
+	num.value = e
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
